447b3033d2bae00de01c0dc2d3dd024403439295
Rustam-Ulaevich <praporglamur@gmail.com>
2026-05-16 20:59:33 +0300

    repetition lesson

--- lesson2.py
+++ lesson2.py
@@ -1,6 +1,6 @@
 money = int(input('сколько у вас денег'))
-if money > 50:
+if money > 50:                           #если 
     print("go to shop")
     print("buy an ice cream")
-else:
+else:                                    #иначе 
     print("today don't have ice cream")
